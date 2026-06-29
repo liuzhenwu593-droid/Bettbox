dba84ca73df47073dc3170f82396e77cdb4adb0f
LITES <liunlong@gmail.com>
2026-06-29 11:00:30 +0800

    重构: 更换多平台脚本覆写QuickJS引擎方案

--- lib/common/js_runtime_manager.dart
+++ lib/common/js_runtime_manager.dart
@@ -1,36 +1,46 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:ffi' as ffi;
-import 'dart:io';
 
-import 'package:flutter_js/flutter_js.dart';
+import 'package:flutter_qjs/flutter_qjs.dart';
 
 class JavaScriptRuntimeManager {
   static Future<Map<String, dynamic>> evaluateScript(
     String scriptContent,
     Map<String, dynamic> config,
   ) async {
-    final runtime = getJavascriptRuntime(xhr: false);
-    final engineId = runtime.getEngineInstanceId();
+    final engine = IsolateQjs();
     try {
       final configJs = json.encode(config);
-      final res = await runtime.evaluateAsync('''
-        $scriptContent
-        main($configJs)
+      final res = await engine.evaluate('''
+        (async function() {
+          $scriptContent
+          return await main($configJs);
+        })();
       ''');
-      if (res.isError) {
-        throw res.stringResult;
+      
+      if (res is Map) {
+        return _deepCastMap(res);
       }
-      final value = switch (res.rawResult is ffi.Pointer) {
-        true => runtime.convertValue<Map<String, dynamic>>(res),
-        false => Map<String, dynamic>.from(res.rawResult),
-      };
-      return value ?? config;
+      return config;
+    } catch (e) {
+      throw 'JS Script Error: $e';
     } finally {
-      JavascriptRuntime.channelFunctionsRegistered.remove(engineId);
-      if (!Platform.isMacOS) {
-        runtime.dispose();
-      }
+      engine.close();
     }
+  }
+
+  static Map<String, dynamic> _deepCastMap(Map dynamicMap) {
+    return dynamicMap.map<String, dynamic>((key, value) {
+      return MapEntry(key.toString(), _deepCastValue(value));
+    });
+  }
+
+  static dynamic _deepCastValue(dynamic value) {
+    if (value is Map) {
+      return _deepCastMap(value);
+    } else if (value is List) {
+      return value.map((e) => _deepCastValue(e)).toList();
+    }
+    return value;
   }
 }
