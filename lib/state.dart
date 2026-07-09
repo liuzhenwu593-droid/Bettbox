2e11913a555d476b92fd0f2c4590368a0229a656
LITES <liunlong@gmail.com>
2026-07-09 22:52:44 +0800

    变更: 针对部分非核心全局配置使用补全而非默认覆写

--- lib/state.dart
+++ lib/state.dart
@@ -522,7 +522,9 @@ class GlobalState {
       }
     }
     rawConfig['external-ui'] = await appPath.uiPath;
-    rawConfig['interface-name'] = '';
+    if (rawConfig['interface-name'] == null) {
+      rawConfig['interface-name'] = '';
+    }
     rawConfig['tcp-concurrent'] = realPatchConfig.tcpConcurrent;
     rawConfig['unified-delay'] = realPatchConfig.unifiedDelay;
     rawConfig['ipv6'] = realPatchConfig.ipv6;
@@ -606,7 +608,12 @@ class GlobalState {
       }
     }
 
-    rawConfig['profile']['store-selected'] = true;
+    if (rawConfig['profile']['store-selected'] == null) {
+      rawConfig['profile']['store-selected'] = true;
+    }
+    if (rawConfig['profile']['store-fake-ip'] == null) {
+      rawConfig['profile']['store-fake-ip'] = true;
+    }
     rawConfig['geox-url'] = realPatchConfig.geoXUrl.toJson();
     rawConfig['global-ua'] = realPatchConfig.globalUa;
     if (rawConfig['hosts'] == null) {
